19c2a76019960d7279cc2580c5467d31c94c60d4
Rim Hourani <rimzhourani@gmail.com>
2024-05-24 19:21:14 +0100

    first commit

---
+++ backend/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) 
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({ storage }).single('file');
+
+app.post('/upload', (req, res) => {
+  upload(req, res, (err) => {
+    if (err instanceof multer.MulterError) {
+      return res.status(500).json(err);
+    } else if (err) {
+      return res.status(500).json(err);
+    }
+    return res.status(200).send('File uploaded successfully');
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
